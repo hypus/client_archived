909edfdc5d354e100b35624bd6cab721bdc91236
Yasin ATEŞ <yasinatesim@gmail.com>
2020-05-21 23:51:04 +0300

    Add storybook feature

---
+++ .storybook/config.js
@@ -0,0 +1,19 @@
+import { configure, addParameters } from "@storybook/react";
+
+addParameters({
+  options: {
+    storySort: (a, b) => {
+      if (a[1].kind === "Welcome") {
+        return -1;
+      }
+
+      return a[1].kind === b[1].kind
+        ? 0
+        : a[1].id.localeCompare(b[1].id, { numeric: true });
+    },
+  },
+});
+
+const req = require.context("../stories", true, /.stories.js$/);
+
+configure(req, module);
