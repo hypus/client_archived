3ba2096636000fd3f6d59d53fb7a08cbc5557f1f
Yasin ATEŞ <yasin.ates@enuygun.com>
2020-05-23 01:27:15 +0300

    feat: add buttons

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,20 @@
 // @flow
 import React from 'react';
-import { useQuery } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
 import { Helmet } from 'react-helmet';
 
-import { withApollo } from '@/utils/apollo';
-
-const POSTS = gql`
-  {
-    allPosts(first: 10) {
-      title
-    }
-  }
-`;
+import { Button } from '@/ui';
 
 function Home() {
-  const { data, loading, error } = useQuery(POSTS);
-
-  if (loading) return <p>LOADING!!</p>;
-  if (error) return <p style={{ color: 'red' }}>{error.message}</p>;
-
   return (
     <>
       <Helmet title="Welcome to Home" />
-      <div className="flex">
-        <ul>
-          {data.allPosts.map((item, index) => (
-            <li key={index.toString()} className="mb-3 bg-primary-hover text-white p-2">
-              {item.title}
-            </li>
-          ))}
-        </ul>
+      <div>
+        <Button outline to="/submit">
+          test
+        </Button>
       </div>
     </>
   );
 }
 
-export default withApollo()(Home);
+export default Home;
