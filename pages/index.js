6a30fecad8c8fc20cab3c3e7303cac61885b1361
Emirhan Engin <me@emirhanengin.com>
2020-05-22 21:40:25 +0300

    background changes

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ function Home() {
       <div className='flex'>
         <ul>
           {data.allPosts.map((item, index) => (
-            <li key={index.toString()} className="mb-3 bg-primary">{item.title}</li>
+            <li key={index.toString()} className="mb-3 bg-primary-hover">{item.title}</li>
           ))}
         </ul>
       </div>
