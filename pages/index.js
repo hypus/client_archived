b6de269be15e039027a0cb1f9b2a4dd52bf37221
Yasin ATEŞ <yasinatesim@gmail.com>
2020-05-31 00:43:45 +0300

    feat: add avatar component

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 
 import { Paragraph, Title } from '@/common';
-import { Textarea, Switcher } from '@/ui';
+import { Avatar, Textarea, Switcher } from '@/ui';
 
 import { AppLayout } from '@/layout';
 
@@ -21,6 +21,10 @@ function Home() {
       <Textarea label="test" />
       {isChecked ? 'checked' : 'not checked'}
       <Switcher label="test et bakalım" isChecked={isChecked} onSelected={(checked) => setIsChecked(checked)} />
+      <Avatar
+        size="lg"
+        source="https://cdn.zeplin.io/5ecc1c5ec6ae1047a3555c37/assets/e33552aa-cd31-4915-8e16-81eff68ad60d@3x.png"
+      />
     </AppLayout>
   );
 }
--- src/components/ui/avatar.js
+++ src/components/ui/avatar.js
@@ -1,11 +1,31 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import cls from 'classnames';
 
-const propTypes = {};
+const propTypes = {
+  extraClassName: PropTypes.string,
+  size: PropTypes.string,
+  source: PropTypes.string,
+};
 
-const defaultProps = {};
+const defaultProps = {
+  size: 'md',
+};
 
-function Avatar() {
-  return <div>Avatar</div>;
+function Avatar({ extraClassName, size, source }) {
+  return (
+    <div
+      className={cls(
+        {
+          'w-12 h-12': size === 'lg',
+          'w-10 h-10': size === 'md',
+          'w-8 h-8': size === 'sm',
+        },
+        extraClassName,
+      )}
+    >
+      <img className="rounded-full" src={source} alt="" />
+    </div>
+  );
 }
 
 Avatar.propTypes = propTypes;
