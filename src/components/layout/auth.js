9230d38eb3fd7437682693d5b009c0e834c8514f
Emirhan Engin <me@emirhanengin.com>
2020-06-04 19:39:56 +0300

    feat(auth): layout

--- src/components/layout/auth.js
+++ src/components/layout/auth.js
@@ -1,11 +1,42 @@
-// import PropTypes from 'prop-types';
+import cls from 'classnames';
+import PropTypes from 'prop-types';
 
-const propTypes = {};
+import Illustration from '@/components/auth/illustration';
+import Header from '@/components/auth-header';
 
-const defaultProps = {};
+const propTypes = {
+  children: PropTypes.node,
+  hasIllustration: PropTypes.bool,
+};
 
-function AuthLayout() {
-  return <div>AuthLayout</div>;
+const defaultProps = {
+  hasIllustration: false,
+};
+
+function AuthLayout({ children, hasIllustration }) {
+  return (
+    <>
+      <Header />
+
+      <div className="container mx-auto mt-25">
+        <div className="flex px-row">
+          {hasIllustration && (
+            <div className="w-5/12 mr-auto px-col">
+              <Illustration />
+            </div>
+          )}
+          <div
+            className={cls('px-col', {
+              'w-5/12': hasIllustration,
+              'w-full md:w-9/12 mx-auto': !hasIllustration,
+            })}
+          >
+            {children}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 AuthLayout.propTypes = propTypes;
